dee3cf2296caf02a7c77552e56dad10fe1bc778b
Thiago Raia <thiagoraia2004@gmail.com>
2023-09-16 11:59:03 -0300

    Salvando

--- ex1.js
+++ ex1.js
@@ -1,2 +1,10 @@
+/* Crie um afunção chamada calcularPreçoComDesconto, essa função vai receber 3 parâmetros - (valor do produto,
+quantidade e uma função callback que aplica o desconto e devolve o valor atualizado)
+*/
+
 import PromptSync from "prompt-sync";
 const prompt = PromptSync()
+
+function calcularPreçoComDesconto(a, b, c, callback) {
+    
+}
---
+++ ex2.js
@@ -0,0 +1,10 @@
+/*
+Utilizando forEach percorra o seguinte array imprimindo apenas os valores que são maiores que 10:
+
+const arrayPrint = [
+    [5, 10, 12],
+    [2, 9, 11],
+    [15, 8, 6]
+]
+*/
+
