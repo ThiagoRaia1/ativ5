1793a8ec0fb036b1d6f8217888a6c9c4e974f774
Thiago Raia <143272411+ThiagoRaia1@users.noreply.github.com>
2023-09-19 14:36:16 -0300

    ex1 resolvido.js

--- ex1.js
+++ ex1.js
@@ -1,11 +1,14 @@
-/* Crie uma função chamada calcularPreçoComDesconto, essa função vai receber 3 parâmetros - (valor do 
-produto, quantidade e uma função callback que aplica o desconto e devolve o valor atualizado)
+/*
+Crie uma função chamada calcularPrecoComDesconto, essa função vai receber 3 parâmetros - (valor do produto, quantidade de produto 
+e uma função callback que aplica o desconto e devolve o valor atualizado) e implementar ao menos uma chamada para a função 
+calcularPrecoComDesconto
 */
 
-import PromptSync from "prompt-sync";
-const prompt = PromptSync()
-
-function calcularPreçoComDesconto(valorProduto, quantidadeProduto, callback) {
-    valorProduto = quantidadeProduto 
+function calcularPrecoComDesconto (valorProduto, quantidadeProduto, callback) {
+  const descontoCompra = valorProduto * quantidadeProduto * 0.10
+  callback(valorProduto * quantidadeProduto - descontoCompra)
 }
 
+calcularPrecoComDesconto(100, 1, (res) => {
+  console.log(res)
+})
